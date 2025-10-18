ab16af44b5ed91c3d2a44291a076a07b725c52ac
Lauren Vo <laurennt.vo@gmail.com>
2025-10-17 20:22:50 -0700

    new

---
+++ global.js
@@ -0,0 +1,100 @@
+console.log('IT’S ALIVE!');
+
+function $$(selector, context = document) {
+  return Array.from(context.querySelectorAll(selector));
+}
+
+// const navLinks = $$("nav a");
+// const currentLink = navLinks.find(a => a.href === location.href);
+// currentLink?.classList.add('current');
+
+let pages = [
+  { url: '', title: 'Home' },
+  { url: 'projects/', title: 'Projects' },
+  { url: 'resume/', title: 'Resume' },
+  { url: 'contact/', title: 'Contact' },
+  { url: 'https://github.com/laurenthanhvo', title: 'GitHub Profile' },
+];
+
+const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
+  ? "/"                
+  : "/portfolio/";       
+
+let nav = document.createElement('nav');
+document.body.prepend(nav);
+
+for (let p of pages) {
+  let url = p.url;
+  let title = p.title;
+
+  if (!url.startsWith('http')) {
+    url = BASE_PATH + url;
+  }
+
+  let a = document.createElement('a');
+  a.href = url;
+  a.textContent = title;
+  nav.append(a);
+
+  a.classList.toggle(
+    'current',
+    a.host === location.host && a.pathname === location.pathname
+  );
+
+  if (a.host !== location.host) {
+    a.target = "_blank";
+    a.rel = "noopener noreferrer";
+  }
+}
+
+document.body.insertAdjacentHTML(
+  'afterbegin',
+  `
+  <label class="color-scheme">
+    Theme:
+    <select>
+      <option value="light dark">Automatic</option>
+      <option value="light">Light</option>
+      <option value="dark">Dark</option>
+    </select>
+  </label>
+  `
+);
+
+const select = document.querySelector('.color-scheme select');
+
+select.addEventListener('input', function (event) {
+  const value = event.target.value;
+  console.log('color scheme changed to', value);
+
+  document.documentElement.style.setProperty('color-scheme', value);
+
+  localStorage.colorScheme = value;
+});
+
+if ('colorScheme' in localStorage) {
+  const saved = localStorage.colorScheme;
+  document.documentElement.style.setProperty('color-scheme', saved);
+  select.value = saved;
+} else {
+  select.value = 'light dark';
+}
+
+const form = document.querySelector('form[action^="mailto:"]');
+
+form?.addEventListener('submit', (event) => {
+  event.preventDefault(); 
+
+  const data = new FormData(form);
+  const parts = [];
+
+  for (let [name, value] of data) {
+    parts.push(`${encodeURIComponent(name)}=${encodeURIComponent(value)}`);
+    console.log(name, value); 
+  }
+
+  const query = parts.join('&');
+  const url = form.action + (query ? `?${query}` : '');
+  
+  location.href = url;
+});
